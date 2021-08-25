a35d0fcd6d95ca571920e92a63cce1b1e98fd91c
sabre3 <84832187+sabre3@users.noreply.github.com>
2021-08-25 17:30:03 -0500

    Added setup.py and a __init__.py for easier module import.

---
+++ setup.py
@@ -0,0 +1,9 @@
+﻿from setuptools import setup
+
+setup(name='filecon',
+      version='0.1.1',
+      description='Python Peiced File Constructor',
+      author='sabre3',
+      packages=['filecon'],
+      install_requires=['PyNaCl', 'ruamel.yaml'],
+     )
